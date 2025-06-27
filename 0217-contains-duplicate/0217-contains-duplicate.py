960dff4e0b648ed3e08a5ace0eeceaac45e64c69
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2025-06-27 14:17:05 -0500

    06-27-2025 0217-contains-duplicate .py

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,6 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
 
+    ##Create a set of nums that will eliminate duplicates
+    ##If we compare the length between created set and the numbers, its easier to find the duplicates
         return (len(set(nums))< len(nums))
       
        
