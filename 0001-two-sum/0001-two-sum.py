b2d3a97dca9f547a76322ff4bc6eef933e023f50
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2025-06-27 16:50:33 -0500

    06-27-2025 0001-two-sum .py

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+          num_map = {}
+
+          for i,num in enumerate(nums):
+            remaining = target-num
+            if remaining in num_map:
+                return [num_map[remaining],i]
+            num_map[num] = i
+    
+
+
+        
