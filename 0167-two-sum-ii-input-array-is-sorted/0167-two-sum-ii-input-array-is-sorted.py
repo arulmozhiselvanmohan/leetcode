22741e8d7711d9b4d9513319afae588be6e95feb
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2025-06-28 14:32:49 -0500

    06-28-2025 0167-two-sum-ii-input-array-is-sorted .py

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.py
@@ -1,11 +1,17 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left=0
+        right = len(numbers)-1
 
-        num_map = {}
-
-        for i,num in enumerate(numbers):
-            remaining = target-num
-            if remaining in num_map:
-                return [num_map[remaining]+1,i+1]
-            num_map[num] = i
+        while(left<len(numbers)):
+            while(left<right):
+                if(target == numbers[left]+numbers[right]):
+                    return([left+1,right+1])
+                else:
+                    right -=1
+            left +=1
+            right = len(numbers)-1
+        
+        return [0]
+        
      
