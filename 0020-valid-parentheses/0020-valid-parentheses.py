d04e6615a303fbdf551c0e38c53b2a1cba608a4d
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2025-06-28 20:48:04 -0500

    06-28-2025 0020-valid-parentheses .py

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -2,9 +2,7 @@ class Solution:
     def isValid(self, s: str) -> bool:
         braces = []
         
-        if len(s) <=1:
-            return False
-    
+         
         for i in range(len(s)):
             if s[i] == "{" or s[i] == "(" or s[i]== "[" :
                 braces.append(s[i])
