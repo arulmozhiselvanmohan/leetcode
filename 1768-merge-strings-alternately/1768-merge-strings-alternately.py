dd8511628b439c9cb2ffd4fcfb9d2861355b7ca4
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2025-06-30 18:39:21 -0500

    06-30-2025 1768-merge-strings-alternately .py

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,16 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        
+        result = ''
+        minlen = min(len(word1),len(word2))
+
+        for i in range(minlen):
+            result += word1[i] + word2[i]
+        
+        result += word1[minlen:]
+        result += word2[minlen:]
+
+        return result
+
+
+        
