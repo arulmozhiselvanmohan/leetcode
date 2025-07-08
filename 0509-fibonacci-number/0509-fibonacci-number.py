43f48d308e7ea20cc526371f52348b3ea817b039
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2025-07-07 21:31:16 -0500

    07-07-2025 0509-fibonacci-number .py

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,14 +1,16 @@
 class Solution:
     def fib(self, n: int) -> int:
-
-        fib = [0] * (n+1)
-        fib[0] = 0
-        if n>0:
-            fib[1] = 1
-
-            for i in range(2,n+1):
-                fib[i] = fib[i-2] + fib[i-1]
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
         
-        return fib[n]
+        prev = 0
+        curr = 1
+
+        for i in range(2,n+1):
+            prev,curr = curr, prev+curr
+        
+        return curr
 
         
