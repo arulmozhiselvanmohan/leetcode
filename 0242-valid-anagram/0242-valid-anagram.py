0ff1da923c42b9157ada0788096274b1e067c164
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2025-07-09 09:26:42 -0500

    07-09-2025 0242-valid-anagram .py

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -7,6 +7,21 @@ class Solution:
         S_dict = Counter(s)
         T_dict = Counter(t)
 
+        #S_dict = {}
+        #T_dict = {}
+
+        #for char in s:
+        #    if char in S_dict:
+        #        S_dict[char] +=1
+        #    else:
+        #        S_dict[char] = 1
+        
+        #for char in t:
+        #    if char in T_dict:
+        #        T_dict[char] +=1
+        #    else:
+        #        T_dict[char] = 1
+
         return (S_dict == T_dict)
          
 
