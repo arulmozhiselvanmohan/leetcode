d4e896dee2791e38ae267a85171ab2f66aed22ac
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2026-01-12 20:45:25 -0500

    01-12-2026 0136-single-number .py

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,6 +1,5 @@
 class Solution:
-    def singleNumber(self, nums: List[int]) -> int:
-        
+    def singleNumber(self, nums: List[int]) -> int:      
         res = 0
 
         for num in nums:
