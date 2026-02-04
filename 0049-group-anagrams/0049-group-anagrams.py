29dd57e0e5b55fc407de5668cfef5d54305160ad
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2026-02-04 14:12:15 -0500

    02-04-2026 0049-group-anagrams .py

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,21 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        anagram = {}
+
+        for index, value in enumerate(strs):
+            pattern = "".join(sorted(value))
+            if pattern in anagram:
+                anagram[pattern].append(value)
+            else:
+                anagram[pattern] = []
+                anagram[pattern].append(value)
+        
+        return list(anagram.values())
+
+       
+
+        
+                
+
+        
