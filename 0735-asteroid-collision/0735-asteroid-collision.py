54adf4ee3123c04b87de6cb19e4488dc20021f55
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2026-02-11 16:26:31 -0500

    02-11-2026 0735-asteroid-collision .py

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -3,14 +3,18 @@ class Solution:
         result = []
         
         for ast in asteroids:
+                #Explodes only when top of stack is positive and asteriod is negative
                 while result and ast < 0 < result[-1]:
                     if result[-1] < -ast:
                         result.pop()
+                        # top of stack explodes
                         continue
                     elif result[-1] == -ast:
                         result.pop()
+                        #Both explode
                         break
                     else:
+                        # asteroid explodes
                         break              
                 else:
                     result.append(ast)           
