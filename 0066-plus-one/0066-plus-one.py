08ad365510bbb49b3c1a016e7ad6eeaffdfef035
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2026-03-04 15:17:43 -0500

    03-04-2026 0066-plus-one .py

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,18 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        number = 0
+        length = len(digits)
+
+        for i,value in enumerate(digits):
+            number += (digits[i]) * (10 ** (length-i-1))
+        
+        number +=1
+
+        phone_number = 2125551234
+        digit_list = [int(digit) for digit in str(number)]
+
+        return digit_list
+
+
+    
+            
