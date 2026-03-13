3234ba57cc69369a77ebd893813bcea70943cfaa
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2026-03-13 17:19:55 -0400

    03-13-2026 1299-replace-elements-with-greatest-element-on-right-side .py

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.py
@@ -0,0 +1,10 @@
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        res = [-1]
+
+        for i in range(len(arr)-1,0,-1):
+            res.insert(0,max(arr[i],res[0]))
+        
+        return res
+                    
+        
