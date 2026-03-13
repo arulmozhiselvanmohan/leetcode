c06e4da46b711e78f3fc504ca9a4601f74d0ba3f
arulmozhiselvanmohan <149892829+arulmozhiselvanmohan@users.noreply.github.com>
2026-03-13 15:15:38 -0400

    03-13-2026 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold .py

---
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numOfSubarrays(self, arr: List[int], k: int, threshold: int) -> int:
+
+        sum_of_sub_array = sum(arr[:k])
+        count=0
+        if sum_of_sub_array/3 >= threshold:
+            count +=1
+
+
+        for i in range(k,len(arr)):
+            sum_of_sub_array = sum_of_sub_array+arr[i]-arr[i-k]
+            if sum_of_sub_array/3 >= threshold:
+                count+=1
+        
+        return count
+
+        
